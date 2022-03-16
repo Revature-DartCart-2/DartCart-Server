863adcf9ff53f13327121f77bd56c3c3938f3b84
Paul Aglipay <paglipay@gmail.com>
2022-03-16 14:37:52 -0700

    edited ProductReview model

--- src/main/java/com/revature/models/ProductReview.java
+++ src/main/java/com/revature/models/ProductReview.java
@@ -1,5 +1,26 @@
 package com.revature.models;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.sun.istack.NotNull;
+
 public class ProductReview {
-    
+	@Id
+	  @GeneratedValue(strategy = GenerationType.IDENTITY)
+	  @Column(name = "product_review_id")
+	  private int id;
+
+	  @NotNull
+	  @Column(unique = true)
+	  private String title;
+
+	  @NotNull
+	  private String comment;
+
+	  @NotNull
+	  private Integer rating;
+
 }
